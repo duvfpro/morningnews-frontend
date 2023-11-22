4491596c4935b513309154fba9aaa8bf939549af
duvfpro <duvfpro@outlook.com>
2023-11-22 16:10:09 +0100

    Modification des fetchs back

--- components/Article.js
+++ components/Article.js
@@ -17,7 +17,7 @@ function Article(props) {
 			return;
 		}
 
-		fetch(`http://localhost:3000/users/canBookmark/${user.token}`)
+		fetch(`https://morningnews-backend-eight.vercel.app/users/canBookmark/${user.token}`)
 			.then(response => response.json())
 			.then(data => {
 				if (data.result && data.canBookmark) {
